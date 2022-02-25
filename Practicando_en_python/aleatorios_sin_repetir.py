1c432efe3832e1f091c2a952d8e9ac66c2b5d4e9
Daniel Stiven Martinez <danimarcast@icloud.com>
2022-02-25 12:30:55 -0500

    Añado el script aleatorios sin repetir

---
+++ Practicando_en_python/aleatorios_sin_repetir.py
@@ -0,0 +1,29 @@
+import numpy as np
+n=int(input("digite un numero: "))
+
+def intentos(l):
+    m=np.arange(n-l)
+    m2=m
+    x=True
+    intento=1
+    while x:
+    
+        x=False
+        np.random.shuffle(m2)
+        for i in range(0,n-l):
+            if(m2[i]==i):
+                x
+            else:
+                intento=intento+1
+#                print(m2)
+                x=True
+     
+   
+    return(intento)
+
+
+print("n","cantidad")
+for i in range(n-2,-1,-1):
+    k=int(intentos(i)/2)
+    print(abs(i-n),k*"* ")
+    
