74f4a22dfb52dcef2bc68e3dda2faa18babb82e6
Daniel Stiven Martinez <danimarcast@icloud.com>
2022-05-19 11:16:28 -0500

    Update conjuntos_cubiertos.py

--- Ejercicios_python/conjuntos_cubiertos.py
+++ Ejercicios_python/conjuntos_cubiertos.py
@@ -5,10 +5,9 @@ def conj_cubierto (S,n,P,m):
             if S[i][0] <= P[j] <= S[i][1]:
                 contador +=1
                 break
-        if contador == n:
-            return True
-        else:
-            return False
+    if contador == n:
+        return True
+    else:
+        return False
 
-
-print(conj_cubierto([[-10,3],[0,2],[2.5,4.1],[5,8],[5,6]],5,[1,3,4,2,8],5))
+print(conj_cubierto([[-10,3],[0,2],[2.5,4.1],[5,8],[4,6]],5,[1,3,4,2,8],5))
